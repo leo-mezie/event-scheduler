b48bf9ed26911f2ff85e408119b3a05ddac3ddb4
chimezie okwuosah <chimezieokwuosah@gmail.com>
2026-07-03 11:07:55 +0100

    made changes to app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import eventsRouter from './routes/events.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
@@ -8,9 +12,9 @@ const PORT = 3000;
 
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('Welcome to the Event Scheduling API');
+  res.send('Welcome to the Event scheduler API');
 });
-
+app.use('/events', eventsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
