6f68b53ae4606174c18e3498462c00c7baf3e574
Olivermarcus2 <marcusolivertwist3@gmail.com>
2026-07-05 15:35:30 +0100

    Add GET and POST event routes

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,9 @@
-
 import express from 'express';
-
+import{getEvents, createEvent} from '../services/eventService.js';
 
 const router = express.Router();
 
-
+router.get('/',getEvents);
+router.post('/',createEvent);
 
 export default router;
