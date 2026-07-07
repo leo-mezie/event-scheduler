c954d1b7501ec1dfa7bcc26c5539a305afdd6c8e
samstar001 <samstarmichael@gmail.com>
2026-07-07 09:59:52 +0100

    fix/delete-expired-events-endpoint and deleteExpiredEvents route order

--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getEvents, getEvent, createEvent, updateEvent, deleteEvent, getEventById, deleteExpiredEvents } from '../services/eventService.js';
+import { getEvents, createEvent, updateEvent, deleteEvent, getEventById, deleteExpiredEvents } from '../services/eventService.js';
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.get('/', getEvents);
 router.get('/:id', getEventById);
 router.post('/', createEvent);
 router.put('/:id', updateEvent);
-router.delete('/:id', deleteEvent);
 router.delete('/expired', deleteExpiredEvents);
+router.delete('/:id', deleteEvent);
 
 export default router;
